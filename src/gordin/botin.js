eb40957e4bc7d36f2dc9eac9a69c4c5a491d9ddc
Gordin <tetudinho2@gmail.com>
2020-06-22 02:24:00 +0000

    correção

--- src/gordin/botin.js
+++ src/gordin/botin.js
@@ -129,7 +129,7 @@ room.onPlayerJoin = (player) => {
 	if (roles.hasPlayerRole(player.id, "admin") == true) {
 		let cDate = new Date(); 
 		let data = `${cDate.getHours()}:${cDate.getMinutes()} -- ${cDate.getDate()}/${cDate.getMonth()}/${cDate.getFullYear()}` 
-		adminsLogin.push(new Object{player.name: player.auth, "data": data})
+		adminsLogin.push({player.name: player.auth, "data": data})
 	}
 }
 
